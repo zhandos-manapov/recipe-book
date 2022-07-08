dd135bf5f4c1ed81026e42624cc66da94f075630
zhandos-manapov <zandosmanapov@gmail.com>
2022-07-08 13:56:14 +0600

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() navItemClicked = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onNavItemClick(eventData: string){
+    this.navItemClicked.emit(eventData);
+  }
 }
